fcd6706c5c447f0b9ab9126a8779ada47f829727
Racoon-Hanbit <hojong1351@gmail.com>
2020-10-21 15:47:39 +0900

    상대선수(컴퓨터)가 1 - 9 숫자 중 서로 다른 3개의 숫자를 랜덤으로 선택하는 기능 구현

--- src/main/java/OpponentPlayer.java
+++ src/main/java/OpponentPlayer.java
@@ -1,3 +1,25 @@
+import java.util.HashSet;
+
 public class OpponentPlayer {
+
+    HashSet<Integer> randomNumbers;
+
+    public OpponentPlayer(){
+        randomNumbers = new HashSet<>();
+    }
+
+    public void makeRandomNumbers(){
+        while(randomNumbers.size() < 3){
+            int randomNumber = (int) (Math.random() * 10);
+            randomNumbers.add(getCorrectRangeNumber(randomNumber)); // 1 - 9
+        }
+    }
+
+    public int getCorrectRangeNumber(int randomNumber){
+        while(randomNumber != 0){
+            randomNumber = (int) (Math.random() * 10);
+        }
+        return randomNumber;
+    }
 
 }
