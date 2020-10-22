7f59b7c65b93cd86464406b790d5efe5c77aa79a
Racoon-Hanbit <hojong1351@gmail.com>
2020-10-22 15:40:33 +0900

    상대(컴퓨터)의 숫자와 도전자의 숫자를 비교하는 기능 테스트

---
+++ src/test/java/ComparatorTest.java
@@ -0,0 +1,75 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ComparatorTest {
+
+    @Test
+    public void given_148_287_then_1ball() {
+        //GIVEN - 테스트케이스, 결과기댓값 준비
+        String opponentNumbers = "148";
+        String challengerNumbers = "287";
+        String expectedResultValue = "1ball";
+
+        //WHEN - 실제 로직 수행
+        String actualResultValue = Comparator.compareNumbers(opponentNumbers, challengerNumbers);
+
+        //THEN - 결과 비교
+        Assert.assertEquals(expectedResultValue, actualResultValue);
+    }
+
+    @Test
+    public void given_248_287_then_1strike_1ball() {
+        //GIVEN - 테스트케이스, 결과기댓값 준비
+        String opponentNumbers = "248";
+        String challengerNumbers = "287";
+        String expectedResultValue = "1strike 1ball";
+
+        //WHEN - 실제 로직 수행
+        String actualResultValue = Comparator.compareNumbers(opponentNumbers, challengerNumbers);
+
+        //THEN - 결과 비교
+        Assert.assertEquals(expectedResultValue, actualResultValue);
+    }
+
+    @Test
+    public void given_348_297_then_out() {
+        //GIVEN - 테스트케이스, 결과기댓값 준비
+        String opponentNumbers = "348";
+        String challengerNumbers = "297";
+        String expectedResultValue = "out";
+
+        //WHEN - 실제 로직 수행
+        String actualResultValue = Comparator.compareNumbers(opponentNumbers, challengerNumbers);
+
+        //THEN - 결과 비교
+        Assert.assertEquals(expectedResultValue, actualResultValue);
+    }
+
+    @Test
+    public void given_325_325_then_3strike() {
+        //GIVEN - 테스트케이스, 결과기댓값 준비
+        String opponentNumbers = "325";
+        String challengerNumbers = "325";
+        String expectedResultValue = "3strike";
+
+        //WHEN - 실제 로직 수행
+        String actualResultValue = Comparator.compareNumbers(opponentNumbers, challengerNumbers);
+
+        //THEN - 결과 비교
+        Assert.assertEquals(expectedResultValue, actualResultValue);
+    }
+
+    @Test
+    public void given_325_235_then_1strike_2ball() {
+        //GIVEN - 테스트케이스, 결과기댓값 준비
+        String opponentNumbers = "325";
+        String challengerNumbers = "235";
+        String expectedResultValue = "1strike 2ball";
+
+        //WHEN - 실제 로직 수행
+        String actualResultValue = Comparator.compareNumbers(opponentNumbers, challengerNumbers);
+
+        //THEN - 결과 비교
+        Assert.assertEquals(expectedResultValue, actualResultValue);
+    }
+}
