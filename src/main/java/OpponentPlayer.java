cc27762fb54610dfcb42ad3bef949922ec64d783
Racoon-Hanbit <hojong1351@gmail.com>
2020-10-22 17:52:58 +0900

    변경사항: OpponentPlayer클래스의 모든 인스턴스 변수를 제거
    게임을 종료 및 재시작할 경우, OpponentPlayer클래스의 randomNumbers(전역변수)의 초기화 필요하다.
    별도의 함수를 사용하지 않고 makeRandomNumbers()함수의 지역변수로 선언 및 정의한다.
    randomNumbers가 지역변수로 할당되면서 인스턴스 변수가 하나도 없으므로 기존의 non-static 함수인 randomNumbers()를 static 함수로
    변경해준다. 이때 이와 관련된 모든 함수 또한 static 함수로 변경해준다.

--- src/main/java/OpponentPlayer.java
+++ src/main/java/OpponentPlayer.java
@@ -2,28 +2,23 @@ import java.util.LinkedHashSet;
 
 public class OpponentPlayer {
 
-    private LinkedHashSet<Integer> randomNumbers;
-
-    public OpponentPlayer(){
-        randomNumbers = new LinkedHashSet<>();
-    }
-
-    public String makeRandomNumbers(){
+    public static String makeRandomNumbers(){
+        LinkedHashSet<Integer> randomNumbers = new LinkedHashSet<>();
         while(randomNumbers.size() < 3){
             int randomNumber = (int) (Math.random() * 10);
             randomNumbers.add(getCorrectRangeNumber(randomNumber));
         }
-        return getRandomNumbers();
+        return getRandomNumbers(randomNumbers);
     }
 
-    private int getCorrectRangeNumber(int randomNumber){
+    private static int getCorrectRangeNumber(int randomNumber){
         while(randomNumber == 0){
             randomNumber = (int) (Math.random() * 10);
         }
         return randomNumber;
     }
 
-    private String getRandomNumbers(){
+    private static String getRandomNumbers(LinkedHashSet<Integer> randomNumbers){
         StringBuffer numbers = new StringBuffer();
         for(Integer number : randomNumbers){
             numbers.append(number);
