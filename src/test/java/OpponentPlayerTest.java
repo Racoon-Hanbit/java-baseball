cb9f0a6b73fbea00c264c09dd379717e708f996d
Racoon-Hanbit <hojong1351@gmail.com>
2020-10-24 00:07:01 +0900

    변경사항: NumbermakerTest 클래스 이름 변경
    Gradle의 명명 규칙 준수를 위해 NumbermakerTest에서  OpponentPlayerTest로 변경

---
+++ src/test/java/OpponentPlayerTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class OpponentPlayerTest {
+
+    @Test
+    public void makeRandomNumber(){
+        OpponentPlayer opponentPlayer = new OpponentPlayer();
+        String temp = opponentPlayer.makeRandomNumbers();
+        boolean check = true;
+        if(temp.length() < 3){
+            check = false;
+        }
+        if(temp.contains("0")){
+            check = false;
+        }
+        assertTrue(check);
+    }
+}
